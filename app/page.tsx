9ec7f88e51993ecea52d756dd1707c376d885c73
rdghostmann <randalchukzwilson@gmail.com>
2025-11-12 14:11:36 +0100

    first commit on Added update II

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
       <HeroSection />
       <StatsSection />
       <CategoriesSection />
+      <About />
       <Services />
       <HowItWorks />
       <MessagingFeature />
